2a02c00611febe9b97f6c99071c84b56443e960f
Mubasher <mobi_20_11@yahoo.com>
2023-04-24 04:58:55 +0200

    useState is working well

--- portfolio-01/src/App.js
+++ portfolio-01/src/App.js
@@ -1,18 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Topbar from './components/topBar/Topbar'
 import Intro from './components/intro/Intro'
 import Portfolio from'./components/portfolio/Portfolio'
 import Works from './components/works/Works'
-import Testimonials from './components/testimonials/Testimonials'
+import Testimonials from './components/testimonials/Testimonios'
 import Contact from './components/contact/Contact'
 import './app.scss'
+import Menu from './components/menu/Menu'
 
 
 
 function App() {
+
+const [menuOpen,setMenuOpen]=useState(false)
+
   return (
     <div className='app' >
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
--- portfolio-01/src/components/topBar/Topbar.jsx
+++ portfolio-01/src/components/topBar/Topbar.jsx
@@ -1,18 +1,31 @@
 import React from 'react'
 import './Topbar.scss'
-function Topbar() {
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+import AttachEmailOutlinedIcon from '@mui/icons-material/AttachEmailOutlined';
+
+
+function Topbar({menuOpen,setMenuOpen}) {
   return (
-    <div className='topBar'>
+    <div className={'topBar ' + (menuOpen && 'active')} >
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className='logo' >CODE FATHER</a>
+          <a href="#intro" className='logo' > CODE FATHER</a>
           <div className="itemContainer">
-            
+            <DeveloperModeIcon className='icon' />
+            <span>+49 123 123 1214</span>
+            </div>  
+            <div className="itemContainer">
+            <AttachEmailOutlinedIcon className='icon' />
+            <span>mobi@codefather.com</span>
             </div>  
         </div>
       
         <div className="right">
-          this is right Side
+          <div className="hamburger" onClick={()=> setMenuOpen(!menuOpen) }>
+            <span className='line1' ></span>
+            <span className='line2' ></span>
+            <span className='line3' ></span>
+          </div>
         </div>
       </div>
     </div>
